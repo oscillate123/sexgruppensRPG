5ad29e4ac343fe2dc4a472cff0241a1bccb9e935
Oliver <ojangholm1@hotmail.com>
2019-12-06 18:11:49 +0100

    Update on Hero class and Monsters Class

--- Hero.py
+++ Hero.py
@@ -1,10 +1,11 @@
 class Hero:
-    def __init__(self, hero_name):
+    def __init__(self, hero_name, initiative, health, attack, agility, score):
         self.hero_name = hero_name
         self.initiative = initiative
         self.health = health
         self.attack = attack
         self.agility = agility
+        self.score = score
 
     def print_stats(self):
         print("-----------------")
@@ -17,16 +18,14 @@ class Hero:
         print(f"Agility : {self.agility}")
         print("-----------------\n")
 
-    def special_skill(self):
+    def special_skill(self, round):
         pass
+
 
 class Knight(Hero):
     def __init__(self, hero_name):
-        self.hero_name = hero_name
-        self.initiative = 5
-        self.health = 9
-        self.attack = 6
-        self.agility = 4
+        super().__init__(hero_name, 5, 9, 6, 4, 0)
+
 
     #Blocks the first attack of every battle.
     def special_skill(self, round):
@@ -36,26 +35,22 @@ class Knight(Hero):
         else:
             return False
 
+
 class Wizard(Hero):
     def __init__(self, hero_name):
-        self.hero_name = hero_name
-        self.initiative = 6
-        self.health = 4
-        self.attack = 9
-        self.agility = 5
+        super().__init__(hero_name, 6, 4, 9, 5, 0)
+
 
     #Always 80% chance to escape a battle
-    def special_skill(self):
+    def special_skill(self, round):
         print("LightBeem")
+
 
 class Rouge(Hero):
     def __init__(self, hero_name):
-        self.hero_name = hero_name
-        self.initiative = 7
-        self.health = 5
-        self.attack = 5
-        self.agility = 7
+        super().__init__(hero_name, 7, 5, 5, 7, 0)
+
 
     #Critical hit. Have a 25% chance of doing 2x damage every turn.
-    def special_skill(self):
+    def special_skill(self, round):
         print("CriticalHit")
