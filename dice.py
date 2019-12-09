c3c51c6f77a53c43b3ac36267c37c0655aa43e67
leffe <rsundh@protonmail.com>
2019-12-09 10:08:05 +0100

    dice function

---
+++ dice.py
@@ -0,0 +1,12 @@
+import random
+
+def n_dice(number_of_dice):
+    dice_sum = 0
+
+    for i in range(0, number_of_dice):
+        dice_sum += random.randint(1,6)
+
+    return dice_sum
+
+#For debug
+print(f"Total dice : {dice_many(6)}")
