9a0be516150ce79e3fe9001a926e6878f19fb1fe
leffe <rsundh@protonmail.com>
2019-12-12 20:49:35 +0100

    update

--- demo_menu.py
+++ demo_menu.py
@@ -6,6 +6,8 @@ from Hero import *
 import json
 from other_functions import*
 
+created_character_list = []
+
 with open("saved_heroes.json", "r") as open_file:
     dict_list = json.load(open_file)
     saved_character_list = []
@@ -54,37 +56,6 @@ def save_character_to_json():
         json.dump(dict_list, close_file)
         close_file.close()
 
-def save_character():
-	if len(created_character_list) != 0:
-		print("Created heroes: ")
-
-		for item in created_character_list:
-			print(item)
-		print("Type in the name of the hero you want to save!")
-
-		name_select = validate_str()
-
-		for item in created_character_list:
-			item = str(item)
-
-			if name_select in item:
-				saved_character_list.append(item)
-
-			save_character_to_json()
-			print("Hero saved!\n")
-	else:
-		print("A hero needs to be created in order to be saved!\n")
-
-def ask_to_save():
-	print_slow(" do you want to save your character at this point?")
-	print_slow("# 1 Save")
-	print_slow("# 2 Continue exploring")
-
-	if (answer == 1):
-		save_character()
-	else:
-		return
-
 def start_menu():
     while True:
         grid_size = 0
@@ -115,20 +86,31 @@ def start_menu():
             exit()
 
 def grid_menu():
-	clear_screen()
-	print_slow(" -----------------")
-	print_slow("A true hero needs a map to explore, please choose a grid size suitable for your adventure")
-	print_slow("The available options are as follows :")
-	print_slow("# 4 for 4x4 grid")
-	print_slow("# 5 for 5x5 grid")
-	print_slow("# 8 for 8x8 grid")
-	print_slow(" -----------------")
-	grid_select = validate_int()
+    grid_selected = False
 
-	if grid_select != 4 and grid_select != 5 and grid_select != 8:
-		print_slow("Wrong input please follow the instructions correctly")
-	else:
-		return grid_select
+    while(grid_selected == False):
+        clear_screen()
+        print_slow(" -----------------")
+        print_slow("A true hero needs a map to explore, please choose a grid size suitable for your adventure")
+        print_slow("The available options are as follows :")
+        print_slow("# 4 for 4x4 grid")
+        print_slow("# 5 for 5x5 grid")
+        print_slow("# 8 for 8x8 grid")
+        print_slow(" -----------------")
+        grid_select = validate_int()
+
+        if grid_select is 4 or grid_select is 5 or grid_select is 8:
+            return grid_select
+
+def save_character():
+    if len(created_character_list) != 0:
+
+        for item in created_character_list:
+            saved_character_list.append(item)
+            print(item)
+
+        save_character_to_json()
+        print("Hero saved!\n")
 
 def choose_hero(hero_class):
 
@@ -147,12 +129,13 @@ def choose_hero(hero_class):
     hero.print_stats()
     print_slow(" -----------------")
 
-    knight.add_hero_dict(dict_list)
-    created_character_list.append(knight)
+    hero.add_hero_dict(dict_list)
+    print(dict_list)
+    created_character_list.append(hero)
 
     choice = input("Press enter to continue or 9 to choose another Hero ")
 
-    return choice, hero.hero_name
+    return choice
 
 def hero_menu():
     while(True):
@@ -173,15 +156,15 @@ def hero_menu():
 
         if (hero_select == 1):
             hero_selected = True
-            choice, hero_name = choose_hero("Knight")
+            choice = choose_hero("Knight")
 
         elif (hero_select == 2):
             hero_selected = True
-            choice, hero_name = choose_hero("Wizard")
+            choice = choose_hero("Wizard")
 
         elif (hero_select == 3):
             hero_selected = True
-            choice, hero_name = choose_hero("Rouge")
+            choice = choose_hero("Rouge")
 
         #Nödlösning med en int och str orkade inte mer...
         if choice == '9':
@@ -194,37 +177,42 @@ def hero_menu():
             return True
 
 def spawn_menu():
-    clear_screen()
     spawn_selected = False
-    spawn_select = 0
-    spawn_coordinates = []
 
-    print_slow(" -----------------")
-    print_slow("Pick a spawn point on the map: ")
-    print_slow("# 1 for NorthWest")
-    print_slow("# 2 for NorthEast")
-    print_slow("# 3 for SouthWest")
-    print_slow("# 4 for SouthEast")
-    print_slow(" -----------------")
+    while(spawn_selected == False):
+        clear_screen()
+        spawn_select = 0
+        spawn_coordinates = []
 
-    spawn_select = validate_int()
+        print_slow(" -----------------")
+        print_slow("Pick a spawn point on the map: ")
+        print_slow("# 1 for NorthWest")
+        print_slow("# 2 for NorthEast")
+        print_slow("# 3 for SouthWest")
+        print_slow("# 4 for SouthEast")
+        print_slow(" -----------------")
 
-    if spawn_select == 1:
-        spawn_point = "NW"
-        spawn_selected = True
-        spawn_coordinates = [0, 0]
-    elif spawn_select == 2:
-        spawn_point = "NE"
-        spawn_selected = True
-        spawn_coordinates = [0, 5]
-    elif spawn_select == 3:
-        spawn_point = "SW"
-        spawn_selected = True
-        spawn_coordinates = [5, 0]
-    elif spawn_select == 4:
-        spawn_point = "SE"
-        spawn_selected = True
-        spawn_coordinates = [5, 5]
+        spawn_select = validate_int()
+
+        if spawn_select == 1:
+            spawn_point = "NW"
+            spawn_selected = True
+            spawn_coordinates = [0, 0]
+        elif spawn_select == 2:
+            spawn_point = "NE"
+            spawn_selected = True
+            spawn_coordinates = [0, 5]
+        elif spawn_select == 3:
+            spawn_point = "SW"
+            spawn_selected = True
+            spawn_coordinates = [5, 0]
+        elif spawn_select == 4:
+            spawn_point = "SE"
+            spawn_selected = True
+            spawn_coordinates = [5, 5]
+        else:
+            print("Wrong choice")
+            continue
 
     if spawn_selected:
         current_run = map(name="demo_run")
@@ -232,8 +220,7 @@ def spawn_menu():
         map_object = current_run.check_cuboid(coordinate=current_run.current_cuboid, option='return')
         print_slow(f"Your spawnpoint is on the {map_object}, at coordinate {current_run.current_cuboid}")
         current_run.print_map()
-        ask_to_save() #REMOVE?
-        save_character() #Added
+        save_character()
         input()
 
 if __name__ == "__main__":
