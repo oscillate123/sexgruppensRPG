e41decb366efed3e63791e1ccb88c35aa93d4459
leffe <rsundh@protonmail.com>
2019-12-13 09:19:21 +0100

    update

--- other_functions.py
+++ other_functions.py
@@ -1,7 +1,11 @@
+
+from os import system, name
+import sys, time
+
 def validate_int():
     while True:
         try:
-            age = int(input("Give me int : "))
+            age = int(input("\n --> "))
         except ValueError:
             print("Wrong format, try again.")
             continue
@@ -10,7 +14,7 @@ def validate_int():
 
 def validate_str():
     while True:
-        name = input("Give me str : ")
+        name = input("\n --> ")
 
         if name.isalpha() or name == "":
             return name.lower().capitalize()
@@ -20,7 +24,7 @@ def validate_str():
 
 def validate_name():
     while True:
-        name = input("Give me name : ")
+        name = input("\n --> ")
 
         if name.isalpha():
             return name.lower().capitalize()
@@ -28,7 +32,22 @@ def validate_name():
             print("Wrong format, try again.")
             continue
 
-#TESTA
-hej2 = validate_int()
-hej3 = validate_str()
-print(validate_name())
+
+def clear_screen():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
+
+def print_slow(str):
+	str = str + "\n"
+	for letter in str:
+		sys.stdout.write(letter)
+		sys.stdout.flush()
+		time.sleep(0.001)
+
+
+if __name__ == "__main__":
+    hej2 = validate_int()
+    hej3 = validate_str()
+    print(validate_name())
