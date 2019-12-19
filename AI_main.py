cb10e98c4125f74c6be4a35d4f9afc6768f273d1
Oscar J Mac <neonode2014@gmail.com>
2019-12-19 17:54:13 +0100

    modified

--- AI_main.py
+++ AI_main.py
@@ -15,30 +15,26 @@ class ai_main:
 		self.ai = AI_class(hero_instance=self.hero, map_instance=self.map)
 		self.fight = Fight(hero=self.hero)
 
-	def print_methods(self, attribute):
+	def text_file(self, attribute):
 		x = inspect.getmembers(attribute, predicate=inspect.ismethod)
-		print()
-		print()
-		print(attribute.__class__.__name__)
+		strings = "\n\n"
+		strings = strings + attribute.__class__.__name__
 		for i in x:
 			for y in i:
 				if type(y) is str:
-					print(y)
+					strings = strings + "\n" + y
+		return strings
 
-	def print_all(self):
-		self.print_methods(self.map)
-		self.print_methods(self.hero)
-		self.print_methods(self.ai)
-		self.print_methods(self.fight)
-
-	def text_file(self):
+	def to_text_file(self, string):
 		with open("struktur.txt", "w+") as open_file:
-			open_file.write(self.print_all())
+			open_file.write(string)
+
+	def text_all(self):
+		super_txt = self.text_file(self.map) + self.text_file(self.hero) + self.text_file(self.ai) + self.text_file(self.fight)
+		self.to_text_file(string=super_txt)
 
 if __name__ == "__main__":
-	input("Vilken hero")
 	main = ai_main()
-	main.print_all()
-	main.text_file()
+	main.text_all()
 
 	
